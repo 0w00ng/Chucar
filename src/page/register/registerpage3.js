02821a6d1a9402a4113ac60971f22cb5b2239b49
0w00ng <k24179906@gmail.com>
2022-02-06 04:26:39 +0900

    DB binding

--- src/page/register/registerpage3.js
+++ src/page/register/registerpage3.js
@@ -10,7 +10,7 @@ export default function RegistePageScreen3({ route,navigation }) {
 
   const {id,password,nickname,email,phone} = route.params;  // Page2에서 보낸 데이터저장
 
-  axios.post(`http://192.168.0.133:3000/signup`, {      // DB전송
+  axios.post(`http://34.64.207.117:3000/signup`, {
         id:id,
         password:password,
         nickname:nickname,
@@ -50,7 +50,7 @@ export default function RegistePageScreen3({ route,navigation }) {
               navigation.navigate('LoginPage')
               navigation.popToTop();              
               }}>
-                <Text style={s.buttontxt3}>로그인 하러가기</Text>
+                <Text style={s.buttontxt3}>로그인 하기</Text>
             </TouchableOpacity>
           </View>
       </View>
